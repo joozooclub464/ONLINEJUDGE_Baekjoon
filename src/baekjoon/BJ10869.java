0cb046e5d289f96e42f850ab86d8ceb4dbce1569
joozooclub <joozooclub464@gmail.com>
2022-11-15 20:51:11 +0900

    BJ10869

---
+++ src/baekjoon/BJ10869.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+public class BJ10869 {
+    public static void main(String[] args) {
+
+        //1부터 10까지의 숫자 중 랜덤
+        //0으로 나눌 수 없기 때문에 +1을 해줌
+        int a = (int)(Math.random()*10)+1;
+        int b = (int)(Math.random()*10)+1;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.println("a+b="+(a+b));
+        System.out.println("a-b="+(a-b));
+        System.out.println("a*b="+(a*b));
+        System.out.println("a/b="+(int)(a/b));
+        System.out.println("a%b="+(int)(a%b));
+    }
+
+}
