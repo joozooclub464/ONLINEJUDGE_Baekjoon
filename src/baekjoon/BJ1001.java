414c26eb96b01c8f3eb25ef9bb44612bfe7f7a8f
joozooclub <joozooclub464@gmail.com>
2022-11-19 16:49:23 +0900

    BJ1001

--- src/baekjoon/BJ1001.java
+++ src/baekjoon/BJ1001.java
@@ -5,15 +5,12 @@ import java.util.Scanner;
 public class BJ1001 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("정수 A를 입력하세요");
         int A = scan.nextInt();
-        System.out.println("정수 B를 입력하세요");
         int B = scan.nextInt();
 
         int C = A + B;
-        System.out.println("A+B="+C);
+        System.out.println(C);
 
 
-//        scan.close();
     }
 }
