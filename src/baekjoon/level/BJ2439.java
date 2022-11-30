356b710f902f8c484cc54dc931898653f570359c
joozooclub <joozooclub464@gmail.com>
2022-11-30 21:49:38 +0900

    별찍기2

---
+++ src/baekjoon/level/BJ2439.java
@@ -0,0 +1,21 @@
+package baekjoon.level;
+//별찍기2
+import java.util.Scanner;
+
+public class BJ2439 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        if(n>=1 && n<=100) {
+            for(int i=0;i<n;i++) {
+                for(int j=n-1;j>i;j--) {
+                    System.out.print(" ");
+                }
+                for(int k=0;k<=i;k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
