7b864a29cd0a924badd7ec9565383b0a673b1909
joozooclub <joozooclub464@gmail.com>
2023-03-01 18:53:14 +0900

    단어 길이 재기

--- src/baekjoon/bronze/B5/BJ2743.java
+++ src/baekjoon/bronze/B5/BJ2743.java
@@ -6,10 +6,7 @@ public class BJ2743 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String alpha = scan.next();
-//        int cnt = 0;
-//        for(int i=0; i<alpha.length();i++) {
-//
-//        }
+
         System.out.println(alpha.length());
     }
 }
