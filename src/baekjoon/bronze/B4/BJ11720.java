661df2163ab8bad221a3f4b4d7dbcfe2c54abd40
joozooclub <joozooclub464@gmail.com>
2023-03-01 18:29:08 +0900

    숫자의 합

--- src/baekjoon/bronze/B4/BJ11720.java
+++ src/baekjoon/bronze/B4/BJ11720.java
@@ -8,10 +8,11 @@ public class BJ11720 {
         int N = scan.nextInt(); //숫자의 개수
         String s = scan.next();
         int sum = 0;
-        String[] strArray = s.split("");
-        for (String s1 : strArray) {
-            sum += Integer.parseInt(s1);
+        String[] strArray = s.split(""); //split 함수에 공백문자를 파라미터로 지정해 하나씩 자름
+        for (String s1 : strArray) { //향상된 for문을 사용하여 sum에 잘라낸 숫자를 더해줌
+            sum += Integer.parseInt(s1); //문자열로 받아왔기 때문에 Integer.parseInt로 int형 형변환을 해줌
         }
         System.out.println(sum);
     }
 }
+//숫자의 개수를 받아온건 사용하지 않고 문제를 풀었는데 저걸 써서 푸는 방법이 있지 않을까? 생각해볼것..
